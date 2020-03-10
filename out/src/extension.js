581c43afe3f4ce10ba664c6f7ddd353759c296a3
Andre Luiz <andre@paconstrushop.com.br>
2020-03-10 16:02:40 -0300

    Adjust on formatting

--- out/src/extension.js
+++ out/src/extension.js
@@ -2,37 +2,55 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var vscode = require("vscode");
 function activate(context) {
-    vscode.commands.registerCommand('extension.advpl-formatter', function () {
-        var activeTextEditor = vscode.window.activeTextEditor;
-        if (activeTextEditor && activeTextEditor.document.languageId === 'advpl') {
-            var currentTab = 0;
-            var document = activeTextEditor.document;
-            var edit = new vscode.WorkspaceEdit();
-            var editor = vscode.workspace.getConfiguration('editor');
-            var file = document.getText().split('\n');
-            var RegexBegin = /^Class|^Method|^\s{0,}For|^\s{0,}if|^\s{0,}While|^\s{0,}BeginSql|^\s{0,}ELSEIF|^\s{0,}ELSE/i;
-            var RegexEnd = /^EndClass|^Return|^\s{0,}Next|^\s{0,}EndIf|^\s{0,}EndDo|^\s{0,}EndSql|^\s{0,}ELSEIF|^\s{0,}ELSE/i;
-            for (var index = 0; index < file.length; index++) {
-                if (RegexEnd.test(file[index])) {
-                    currentTab -= editor.tabSize;
-                }
-                if (currentTab < 0) {
-                    currentTab = 0;
-                }
-                edit.replace(document.uri, new vscode.Range(index, 0, index, file[index].length), "" + ' '.repeat(currentTab) + file[index].toString().trim());
-                if (RegexBegin.test(file[index])) {
-                    currentTab += editor.tabSize;
-                }
-                if (index === file.length - 1) {
-                    if (file[index] !== '') {
-                        edit.insert(document.uri, document.lineAt(document.lineCount - 1).range.end, '\n');
+    vscode.languages.registerDocumentFormattingEditProvider('advpl', {
+        provideDocumentFormattingEdits: function (document) {
+            var activeTextEditor = vscode.window.activeTextEditor;
+            if (activeTextEditor && activeTextEditor.document.languageId === 'advpl') {
+                var currentTab = 0;
+                var document_1 = activeTextEditor.document;
+                var edit = new vscode.WorkspaceEdit();
+                var editor = vscode.workspace.getConfiguration('editor');
+                var file = document_1.getText().split('\n');
+                var RegexBegin = /^\s{0,}Class|^\s{0,}Method|^\s{0,}For|^\s{0,}if|^\s{0,}While|^\s{0,}BeginSql|^\s{0,}ELSEIF|^\s{0,}ELSE/i;
+                var RegexEnd = /^\s{0,}EndClass|^\s{0,}Return|^\s{0,}Next|^\s{0,}EndIf|^\s{0,}EndDo|^\s{0,}EndSql|^\s{0,}ELSEIF|^\s{0,}ELSE/i;
+                for (var index = 0; index < file.length; index++) {
+                    if (RegexEnd.test(file[index])) {
+                        currentTab -= editor.tabSize;
                     }
-                    var currentTab = 0;
-                    return vscode.workspace.applyEdit(edit);
+                    if (currentTab < 0) {
+                        currentTab = 0;
+                    }
+                    if (file[index].toString().trim() !== '') {
+                        edit.replace(document_1.uri, new vscode.Range(index, 0, index, file[index].length), "" + ' '.repeat(currentTab) + formattetSring(file[index].toString().trim()));
+                    }
+                    if (file[index].toString().trim() === '') {
+                        edit.replace(document_1.uri, new vscode.Range(index, 0, index, file[index].length), "" + file[index].toString().trim());
+                    }
+                    if (RegexBegin.test(file[index])) {
+                        currentTab += editor.tabSize;
+                    }
+                    if (index === file.length - 1) {
+                        if (file[index] !== '') {
+                            edit.insert(document_1.uri, document_1.lineAt(document_1.lineCount - 1).range.end, '\n');
+                        }
+                        var currentTab = 0;
+                        vscode.workspace.applyEdit(edit);
+                        return [];
+                    }
                 }
             }
         }
     });
 }
 exports.activate = activate;
+function formattetSring(str) {
+    return str.replace(/([^"]+)|("[^"]+")/g, function ($0, $1, $2) {
+        if ($1) {
+            return $1.replace(/\s{1,}/g, " ");
+        }
+        else {
+            return $2;
+        }
+    });
+}
 //# sourceMappingURL=extension.js.map
--- src/extension.ts
+++ src/extension.ts
@@ -3,39 +3,57 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 
-  vscode.commands.registerCommand('extension.advpl-formatter', () => {
-    const { activeTextEditor } = vscode.window;
+  vscode.languages.registerDocumentFormattingEditProvider('advpl', {
+    provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
+      const { activeTextEditor } = vscode.window;
 
+      if (activeTextEditor && activeTextEditor.document.languageId === 'advpl') {
+        var currentTab = 0;
+        const { document } = activeTextEditor;
+        const edit = new vscode.WorkspaceEdit();
+        const editor = vscode.workspace.getConfiguration('editor');
 
-    if (activeTextEditor && activeTextEditor.document.languageId === 'advpl') {
-      var currentTab = 0;
-      const { document } = activeTextEditor;
-      const edit = new vscode.WorkspaceEdit();
-      const editor = vscode.workspace.getConfiguration('editor');
+        const file = document.getText().split('\n');
+        const RegexBegin = /^\s{0,}Class|^\s{0,}Method|^\s{0,}For|^\s{0,}if|^\s{0,}While|^\s{0,}BeginSql|^\s{0,}ELSEIF|^\s{0,}ELSE/i;
+        const RegexEnd = /^\s{0,}EndClass|^\s{0,}Return|^\s{0,}Next|^\s{0,}EndIf|^\s{0,}EndDo|^\s{0,}EndSql|^\s{0,}ELSEIF|^\s{0,}ELSE/i;
 
-      const file = document.getText().split('\n');
-      const RegexBegin = /^Class|^Method|^\s{0,}For|^\s{0,}if|^\s{0,}While|^\s{0,}BeginSql|^\s{0,}ELSEIF|^\s{0,}ELSE/i;
-      const RegexEnd = /^EndClass|^Return|^\s{0,}Next|^\s{0,}EndIf|^\s{0,}EndDo|^\s{0,}EndSql|^\s{0,}ELSEIF|^\s{0,}ELSE/i;
-
-      for (let index = 0; index < file.length; index++) {
-        if (RegexEnd.test(file[index])) {
-          currentTab -= editor.tabSize;
-        }
-        if (currentTab < 0) {
-          currentTab = 0;
-        }
-        edit.replace(document.uri, new vscode.Range(index, 0, index, file[index].length), `${' '.repeat(currentTab)}${file[index].toString().trim()}`);
-        if (RegexBegin.test(file[index])) {
-          currentTab += editor.tabSize;
-        }
-        if (index === file.length - 1) {
-          if (file[index] !== '') {
-            edit.insert(document.uri, document.lineAt(document.lineCount - 1).range.end, '\n');
+        for (let index = 0; index < file.length; index++) {
+          if (RegexEnd.test(file[index])) {
+            currentTab -= editor.tabSize;
           }
-          var currentTab = 0;
-          return vscode.workspace.applyEdit(edit);
+          if (currentTab < 0) {
+            currentTab = 0;
+          }
+          if (file[index].toString().trim() !== '') {
+            edit.replace(document.uri, new vscode.Range(index, 0, index, file[index].length),
+              `${' '.repeat(currentTab)}${formattetSring(file[index].toString().trim())}`);
+          }
+          if (file[index].toString().trim() === '') {
+            edit.replace(document.uri, new vscode.Range(index, 0, index, file[index].length), `${file[index].toString().trim()}`);
+          }
+          if (RegexBegin.test(file[index])) {
+            currentTab += editor.tabSize;
+          }
+          if (index === file.length - 1) {
+            if (file[index] !== '') {
+              edit.insert(document.uri, document.lineAt(document.lineCount - 1).range.end, '\n');
+            }
+            var currentTab = 0;
+            vscode.workspace.applyEdit(edit)
+            return [];
+          }
         }
       }
+    }
+  });
+}
+
+function formattetSring(str) {
+  return str.replace(/([^"]+)|("[^"]+")/g, function ($0, $1, $2) {
+    if ($1) {
+      return $1.replace(/\s{1,}/g, " ");
+    } else {
+      return $2;
     }
   });
 }
